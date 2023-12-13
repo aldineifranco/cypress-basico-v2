b9aadd5b16062cf4ee4d635117f03246784cfc2e
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-12-13 09:32:25 -0300

    comandos personalizados

--- cypress/integration/CAC-TAC.spec.js
+++ cypress/integration/CAC-TAC.spec.js
@@ -37,10 +37,53 @@ describe("Central de Atendimento ao Cliente TAT", () => {
   });
 
   it("campo telefone continua vazio quando preenchido com valor não numérico", () => {
-    cy.get("#phone").type("asdfalkjhlkj").should("have.value", "")
+    cy.get("#phone")
+      .type("asdfalkjhlkj")
+      .should("have.value", "");
   });
 
-  it.only("", () => {
-    
+  it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
+    cy.get("#firstName").type("Aldinei");
+    cy.get("#lastName").type("Franco");
+    cy.get("#email").type("exemplo@gmail,com");
+    cy.get("#phone-checkbox").click();
+    cy.get("#open-text-area").type("Teste");
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  it("preenche e limpa os campos nome, sobrenome, email e telefone", () => {
+    cy.get("#firstName")
+      .type("Aldinei")
+      .should("have.value", "Aldinei")
+      .clear()
+      .should("have.value", "");
+    cy.get("#lastName")
+      .type("Franco")
+      .should("have.value", "Franco")
+      .clear()
+      .should("have.value", "");
+    cy.get("#email")
+      .type("exemplo@gmail,com")
+      .should("have.value", "exemplo@gmail,com")
+      .clear()
+      .should("have.value", "");
+    cy.get("#phone")
+      .type("999966666")
+      .should("have.value", "999966666")
+      .clear()
+      .should("have.value", "");
+  });
+
+  it("exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", () => {
+    cy.get(".button[type='submit']").click();
+    cy.get(".error").should("be.visible");
+  });
+
+  it("envia o formuário com sucesso usando um comando customizado", () => {
+    cy.fillMandatoryFieldsAndSubmit();
+
+    cy.get(".success").should("be.visible");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,10 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add(
+  "fillMandatoryFieldsAndSubmit",
+  () => {
+    cy.get("#firstName").type("Aldinei");
+    cy.get("#lastName").type("Franco");
+    cy.get("#email").type("exemplo@gmail.com");
+    cy.get("#open-text-area").type("longText");
+    cy.get('button[type="submit"]').click();
+  }
+);
