43dba6131b2541317e46ba6b5592383ab4c88888
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-12-13 08:38:02 -0300

    teste 03 exercício

--- cypress/integration/CAC-TAC.spec.js
+++ cypress/integration/CAC-TAC.spec.js
@@ -27,12 +27,20 @@ describe("Central de Atendimento ao Cliente TAT", () => {
     cy.get(".success").should("be.visible");
   });
 
-  it.only("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
+  it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
     cy.get("#firstName").type("Aldinei");
     cy.get("#lastName").type("Franco");
     cy.get("#email").type("exemplo@gmail,com");
     cy.get("#open-text-area").type("Teste");
     cy.get('button[type="submit"]').click();
     cy.get(".error").should("be.visible");
+  });
+
+  it("campo telefone continua vazio quando preenchido com valor não numérico", () => {
+    cy.get("#phone").type("asdfalkjhlkj").should("have.value", "")
+  });
+
+  it.only("", () => {
+    
   });
 });
