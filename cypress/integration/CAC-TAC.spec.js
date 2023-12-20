e2e0738885e86ccfd459525b12e4fe9891b18e52
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-12-20 09:33:55 -0300

    radio

--- cypress/integration/CAC-TAC.spec.js
+++ cypress/integration/CAC-TAC.spec.js
@@ -93,14 +93,32 @@ describe("Central de Atendimento ao Cliente TAT", () => {
       .should("have.value", "youtube");
   });
 
-  it.only("seleciona um produto (Mentoria) por seu valor (value)", () => {
+  it("seleciona um produto (Mentoria) por seu valor (value)", () => {
     cy.get("#product")
       .select("mentoria")
       .should("have.value", "mentoria");
   });
+
   it("seleciona um produto (Blog) por seu índice", () => {
     cy.get("#product")
       .select(1)
       .should("have.value", "blog");
   });
+
+  it("marca o tipo de atendimento 'Feedback'", () => {
+    cy.get(
+      'input[type="radio"][value="feedback"]'
+    )
+      .check()
+      .should("have.value", "feedback");
+  });
+  it("marca cada tipo de atendimento", () => {
+    cy.get('input[type="radio"]')
+      .should("have.length", 3)
+      .each(($radio) => {
+        cy.wrap($radio).check()
+        cy.wrap($radio).should("be.checked")
+      });
+  });
+
 });
