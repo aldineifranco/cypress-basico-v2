530533fc6212c7e6c6bef33f69df5481aa162333
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-12-20 11:51:51 -0300

    upload de arquivos

--- cypress/integration/CAC-TAC.spec.js
+++ cypress/integration/CAC-TAC.spec.js
@@ -112,34 +112,74 @@ describe("Central de Atendimento ao Cliente TAT", () => {
       .check()
       .should("have.value", "feedback");
   });
+
   it("marca cada tipo de atendimento", () => {
     cy.get('input[type="radio"]')
       .should("have.length", 3)
       .each(($radio) => {
-        cy.wrap($radio).check()
-        cy.wrap($radio).should("be.checked")
+        cy.wrap($radio).check();
+        cy.wrap($radio).should("be.checked");
       });
   });
   //each e wrap passa pelos três radios
 
   it("marca ambos os checkboxes", () => {
     cy.get('input[type="checkbox"]')
-    .check()
-    .should("be.checked")
-    .last()
-    .uncheck()
-    .should('not.be.checked')
+      .check()
+      .should("be.checked")
+      .last()
+      .uncheck()
+      .should("not.be.checked");
   });
   //pega os dois inputs checkbox e checa, depois pega o último com last e desmarca. Por fim verifica com should se está desmarcado.
 
-   it.only("--exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
-     cy.get("#firstName").type("Aldinei");
-     cy.get("#lastName").type("Franco");
-     cy.get("#email").type("exemplo@gmail,com");
-     cy.get("#phone-checkbox").check();
-     cy.get("#open-text-area").type("Teste");
-     cy.contains("button", "Enviar").click();
+  it("--exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
+    cy.get("#firstName").type("Aldinei");
+    cy.get("#lastName").type("Franco");
+    cy.get("#email").type("exemplo@gmail,com");
+    cy.get("#phone-checkbox").check();
+    cy.get("#open-text-area").type("Teste");
+    cy.contains("button", "Enviar").click();
 
-     cy.get(".error").should("be.visible");
-   });
+    cy.get(".error").should("be.visible");
+  });
+
+  it("seleciona um arquivo da pasta fixtures", () => {
+    cy.get("input[type='file']#file-upload")
+      .should("not.have.value")
+      .selectFile(
+        "./cypress/fixtures/example.json"
+      )
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal(
+          "example.json"
+        );
+      });
+  });
+
+  it("seleciona um arquivo simulando um drag-and-drop", () => {
+    cy.get("input[type='file']#file-upload")
+      .should("not.have.value")
+      .selectFile(
+        "./cypress/fixtures/example.json",
+        { action: "drag-drop" }
+      )
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal(
+          "example.json"
+        );
+      });
+  });
+
+  it("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", () => {
+    cy.fixture("example.json").as("sampleFile");
+    cy.get("input[type='file']")
+      .selectFile("@sampleFile")
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal(
+          "example.json"
+        );
+      });
+  });
+
 });
